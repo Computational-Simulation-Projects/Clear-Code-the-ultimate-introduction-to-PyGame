f96a9184cfa14173f4cf9a4f852d06b4d1f50cde
Ujjal <khadkauzal@gmail.com>
2021-12-13 17:58:38 +0545

    implemented-basic-animations

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ ground_surface = pygame.image.load('./graphics/Ground.png').convert()
 # here ani-aliasing is turned off since it is a pixel art
 text_surface = test_font.render('Runner Game', False, 'Black')
 
+# here convert() is not needed since the sprite needs transparent background
+# convert_alpha() considers th transparency / alpha values too
+snail_surface = pygame.image.load(
+    './graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 700
+
 # create the game loop
 game_running = True
 while game_running:
@@ -36,13 +42,21 @@ while game_running:
             game_running = False
 
     # draw all the elements
+    # fill the screen with black to reset the screen every frame
+    screen.fill('Black')
     # blit (Block-Image-Transfer) is used to draw the surface on another
     # blit works on the order of the interpreter... top to bottom
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (width // 2 - text_surface.get_width() // 2, 50))
+    screen.blit(snail_surface, (snail_x_pos, 265))
 
     # update everything
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    else:
+        snail_x_pos -= 4
+
     pygame.display.update()  # updates the display
     clock.tick(60)  # 60 frames per second
 
