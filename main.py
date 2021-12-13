2f08e99f002ac2aa1d6e71c87c54b5ea397faca1
Ujjal <khadkauzal@gmail.com>
2021-12-13 17:07:54 +0545

    imported-media-and-graphics-files-and-displayed-images-and-text-using-surfaces

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ pygame.display.set_caption('Runner')
 # create a clock
 clock = pygame.time.Clock()
 
+# create a font
+test_font = pygame.font.Font('./font/Pixeltype.ttf', 50)
+
+# create a surface
+# .convert() converts the surface to the display format... it's fast
+sky_surface = pygame.image.load('./graphics/Sky.png').convert()
+ground_surface = pygame.image.load('./graphics/Ground.png').convert()
+# True = anti-aliased && aniti-aliased is slow but looks better by smoothening the jagged edges by averaging pixels
+# here ani-aliasing is turned off since it is a pixel art
+text_surface = test_font.render('Runner Game', False, 'Black')
+
 # create the game loop
 game_running = True
 while game_running:
@@ -25,6 +36,12 @@ while game_running:
             game_running = False
 
     # draw all the elements
+    # blit (Block-Image-Transfer) is used to draw the surface on another
+    # blit works on the order of the interpreter... top to bottom
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (width // 2 - text_surface.get_width() // 2, 50))
+
     # update everything
     pygame.display.update()  # updates the display
     clock.tick(60)  # 60 frames per second
